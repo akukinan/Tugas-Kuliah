8f3cb8195d210cd0195a005bdb4e610ef19586df
akukinan <yosafatkinantan@gmail.com>
2024-11-11 15:33:02 +0700

    reworked

--- java/tugas2_waktu_ujian.java
+++ java/tugas2_waktu_ujian.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class waktu_ujian {
+public class tugas2_waktu_ujian {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
